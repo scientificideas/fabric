f16791ce3af8ffef3950f93e44ff6efa2a38a946
Aleksei Markov <amarkov@gramvault.com>
2020-12-01 17:16:11 +0200

    [BFT-6] Block's hash validation is added.
    Signed-off-by: Aleksei Markov <amarkov@gramvault.com>

--- orderer/consensus/smartbft/verifier.go
+++ orderer/consensus/smartbft/verifier.go
@@ -6,4 +6,26 @@ SPDX-License-Identifier: Apache-2.0
 
 package smartbft
 
+import (
+	"encoding/hex"
+
+	cb "github.com/hyperledger/fabric-protos-go/common"
+	"github.com/hyperledger/fabric/protoutil"
+	"github.com/pkg/errors"
+)
+
 type NodeIdentitiesByID map[uint64][]byte
+
+func verifyHashChain(block *cb.Block, prevHeaderHash string) error {
+	thisHdrHashOfPrevHdr := hex.EncodeToString(block.Header.PreviousHash)
+	if prevHeaderHash != thisHdrHashOfPrevHdr {
+		return errors.Errorf("previous header hash is %s but expected %s", thisHdrHashOfPrevHdr, prevHeaderHash)
+	}
+
+	dataHash := hex.EncodeToString(block.Header.DataHash)
+	actualHashOfData := hex.EncodeToString(protoutil.BlockDataHash(block.Data))
+	if dataHash != actualHashOfData {
+		return errors.Errorf("data hash is %s but expected %s", dataHash, actualHashOfData)
+	}
+	return nil
+}
