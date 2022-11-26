0c4b435f939a2dad6cb35bbce00173320ed1a792
Fred Partanskiy <pfi79@mail.ru>
2022-11-26 18:51:24 +0300

    up 2.5
    Signed-off-by: Fred Partanskiy <pfi79@mail.ru>

--- vendor/github.com/SmartBFT-Go/consensus/smartbftprotos/generate.go
+++
@@ -1,4 +0,0 @@
-package smartbftprotos
-
-//go:generate protoc --go_out=plugins=grpc,paths=source_relative:. logrecord.proto
-//go:generate protoc --go_out=plugins=grpc,paths=source_relative:. messages.proto
