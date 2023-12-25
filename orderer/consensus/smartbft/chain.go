7729698f66133ba72a08b4facc17358f79c18a86
pfi79 <pfi79@mail.ru>
2023-12-25 15:06:17 +0300

    removed the request from the pool if the check showed that the proposal is bad (#4572)
    Signed-off-by: Fedor Partanskiy <pfi79@mail.ru>
    
    (cherry picked from commit 073b57009f56254f26b9a0fa7f72202d72d9a659)
    Signed-off-by: Fedor Partanskiy <pfi79@mail.ru>

--- orderer/consensus/smartbft/chain.go
+++ orderer/consensus/smartbft/chain.go
@@ -305,6 +305,13 @@ func (c *BFTChain) pruneCommittedRequests(block *cb.Block) {
 	wg.Wait()
 }
 
+func (c *BFTChain) pruneBadRequests() {
+	c.consensus.Pool.Prune(func(req []byte) error {
+		_, err := c.consensus.Verifier.VerifyRequest(req)
+		return err
+	})
+}
+
 func (c *BFTChain) submit(env *cb.Envelope, configSeq uint64) error {
 	reqBytes, err := proto.Marshal(env)
 	if err != nil {
@@ -556,6 +563,7 @@ func (c *BFTChain) updateRuntimeConfig(block *cb.Block) types.Reconfig {
 	c.RuntimeConfig.Store(newRTC)
 	if protoutil.IsConfigBlock(block) {
 		c.Comm.Configure(c.Channel, newRTC.RemoteNodes)
+		c.pruneBadRequests()
 	}
 
 	membershipDidNotChange := reflect.DeepEqual(newRTC.Nodes, prevRTC.Nodes)
